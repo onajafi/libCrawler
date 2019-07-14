ef1f1821a7f2f1755bd45afb3145bb4bc3f1fcff
dot_blue <onajafi141@gmail.com>
2019-07-14 18:03:18 -0400

    Implement the CasperJS extender

--- crawlers/EXT.js
+++ crawlers/EXT.js
@@ -14,12 +14,32 @@ var file_input = fs.read('../tmp/'+casper.cli.args[0]);
 var parsed_input_JSON = JSON.parse(file_input);
 
 
+//Regex
+var date_regex = /[0-9]{4}[/][0-9]{1,2}[/][0-9]{1,2}/;
+
+
+
 //Functions
-function extendTime(ref,link){
-    ref.thenOpen(link)
-    ref.capture('navigation.png');
 
+//This function enters the page and clicks the button
+// to extend the return time of the reserved book.
+function extendTime(ref,link) {
 
+    temp_output = true;
+
+    casper.thenOpen(link)
+        .then(function() {
+            casper.capture('navigation.png');
+        })
+        .thenClick('input.btn.btn-primary')
+        .then(function() {
+            if(this.exists('div#errorMessages.error')){
+                this.echo("Error in extending the time!!!");
+                temp_output = false;
+            }
+        });
+
+    return temp_output;
 }
 
 function cleanText(rawText){
@@ -28,6 +48,10 @@ function cleanText(rawText){
     return rawText
 }
 
+function extract_json_file(){
+    var output_filename = 'output_EXT_'+parsed_input_JSON.chat_id + '.json';
+    fs.write('../tmp/'+output_filename, JSON.stringify(output_for_JSON), 'w');
+}
 //Main
 
 casper.start('http://library.sharif.ir/cas/login?renew=true&service=http://library.sharif.ir:80/parvan/j_spring_cas_security_check');
@@ -52,10 +76,6 @@ casper.then(function() {
     this.echo(parsed_input_JSON["user"]);
     this.echo(this.sendKeys('input#username', parsed_input_JSON["user"]));
     this.echo(this.sendKeys('input#password', parsed_input_JSON["pass"]));
-    // this.echo("+++++++++++++++++++++++++++++++++++++++++++++++++");
-    // require('utils').dump(this.getElementInfo('input#username'));
-    // this.echo("_________________________________________________");
-    // require('utils').dump(this.getElementInfo('input.button'));
 })
     .thenClick('input.button')
     .thenOpen("http://library.sharif.ir/parvan/borrowed.do?action=list&listViewStatus=active")
@@ -69,24 +89,34 @@ casper.then(function() {
                 this.echo("Found row #" + i);
                 // elem = this.getElementInfo(rowSelector);
                 tmpJSONrow["number"] = cleanText(this.getElementInfo(rowSelector + "> td:nth-child(1)")["text"]);
-                tmpJSONrow["number"] = JSON.stringify(cleanText(this.getElementInfo(rowSelector + "> td:nth-child(2)")["text"]));
-                // tmpJSONrow["number"] = JSON.stringify(cleanText(this.getElementInfo(rowSelector + "> td:nth-child(3)")["text"]));
-                // tmpJSONrow["number"] = JSON.stringify(cleanText(this.getElementInfo(rowSelector + "> td:nth-child(4)")["text"]));
-                // tmpJSONrow["number"] = JSON.stringify(cleanText(this.getElementInfo(rowSelector + "> td:nth-child(5)")["text"]));
-                // tmpJSONrow["number"] = JSON.stringify(cleanText(this.getElementInfo(rowSelector + "> td:nth-child(6)")["text"]));
-                // tmpJSONrow["number"] = JSON.stringify(cleanText(this.getElementInfo(rowSelector + "> td:nth-child(7)")["text"]));
-                // tmpJSONrow["number"] = JSON.stringify(cleanText(this.getElementInfo(rowSelector + "> td:nth-child(8)")["text"]));
-                // tmpJSONrow["number"] = JSON.stringify(cleanText(this.getElementInfo(rowSelector + "> td:nth-child(9)")["text"]));
-                // tmpJSONrow["number"] = JSON.stringify(cleanText(this.getElementInfo(rowSelector + "> td:nth-child(10)")["text"]));
-                // tmpJSONrow["num"] = elem
-                require('utils').dump(this.getElementInfo(rowSelector + "> td:nth-child(2) > a")["attributes"]["href"]);
-                extendTime(this,
-                    "http://library.sharif.ir" + this.getElementInfo(rowSelector + "> td:nth-child(2) > a")["attributes"]["href"])
+                //tmpJSONrow["number"] = JSON.stringify(cleanText(this.getElementInfo(rowSelector + "> td:nth-child(2)")["text"]);
+                tmpJSONrow["title"] = cleanText(this.getElementInfo(rowSelector + "> td:nth-child(3)")["text"]);
+                tmpJSONrow["borrowingDate"] = cleanText(this.getElementInfo(rowSelector + "> td:nth-child(4)")["text"].match(date_regex)[0]);
+                tmpJSONrow["returnDate"] = cleanText(this.getElementInfo(rowSelector + "> td:nth-child(5)")["text"].match(date_regex)[0]);
+                tmpJSONrow["bookID"] = cleanText(this.getElementInfo(rowSelector + "> td:nth-child(6)")["text"]);
+                tmpJSONrow["bookNAV"] = cleanText(this.getElementInfo(rowSelector + "> td:nth-child(7)")["text"]);
+                tmpJSONrow["placeItBelongs"] = cleanText(this.getElementInfo(rowSelector + "> td:nth-child(8)")["text"]);
+                // tmpJSONrow["number"] = cleanText(this.getElementInfo(rowSelector + "> td:nth-child(9)")["text"]);
+                tmpJSONrow["status"] = cleanText(this.getElementInfo(rowSelector + "> td:nth-child(10)")["text"]);
+
+                //Click the link
+                if(parsed_input_JSON["extend"]) {
+                    extendResult = extendTime(this,
+                        "http://library.sharif.ir" + this.getElementInfo(rowSelector + "> td:nth-child(2) > a")["attributes"]["href"]
+                    );
+                } else {
+                    tmpJSONrow["extended_successfully"] = false;
+                }
+
+                output_for_JSON["table"].push(tmpJSONrow)
+
             } else {
                 break
             }
         }
-    })
+    }).then(function () {
+        extract_json_file();
+    });
 
 
 
---
+++ inits.py
@@ -0,0 +1,11 @@
+# This file contains the classified settings of the bot...
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import telebot
+
+tel_token = "<###THE BOTS TOKEN###>" #TEST token
+
+feedBack_target_chat = <CHAT_ID> #The chat where all the FeedBacks will be forwarded, Make sure the bot has access to this chat
+
+bot = telebot.TeleBot(tel_token)
